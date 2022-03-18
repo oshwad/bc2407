cc5484f0a844c8c188ec7eff43c25a433f4a9d52
oshwad <lkl.faith05@gmail.com>
2022-03-18 15:54:07 +0800

    change

--- 4. neural network.R
+++ 4. neural network.R
@@ -1,7 +1,9 @@
 library(data.table)
+library("ROSE")
 library(neuralnet)
 
-dt2 <- fread('dt2-cleaned.csv', stringsAsFactors = T)
-sapply(dt2, class)
-dt2[,c('admission_type_id', 'admission_source_id', 'readmitted', 'repeat_patient')] <- lapply(dt1[,c('admission_type_id', 'admission_source_id', 'readmitted', 'repeat_patient')], factor)
-sapply(dt2, class)
+setwd("~/GitHub/bc2407")
+dt1 <- fread('dt1-cleaned.csv', stringsAsFactors = T)
+dt1[,c('admission_type_id', 'admission_source_id', 'readmitted', 'repeat_patient')] <- lapply(dt1[,c('admission_type_id', 'admission_source_id', 'readmitted', 'repeat_patient')], factor)
+dt2 <- ROSE(readmitted~., data = dt1, N = nrow(dt1), seed=111)$data
+table(dt2$readmitted)
