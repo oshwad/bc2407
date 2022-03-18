5e7a2914aac688384925acc1044f5abfd5f92d7e
oshwad <lkl.faith05@gmail.com>
2022-03-18 23:30:32 +0800

    random forest

--- 5. random forest.R
+++ 5. random forest.R
@@ -1,9 +1,41 @@
 library(data.table)
 library("ROSE")
+library(caTools)
 library(randomForest)
+library(caret)
 
 setwd("~/GitHub/bc2407")
 dt1 <- fread('dt1-cleaned.csv', stringsAsFactors = T)
 dt1[,c('admission_type_id', 'admission_source_id', 'readmitted', 'repeat_patient')] <- lapply(dt1[,c('admission_type_id', 'admission_source_id', 'readmitted', 'repeat_patient')], factor)
 dt2 <- ROSE(readmitted~., data = dt1, N = nrow(dt1), seed=111)$data
 table(dt2$readmitted)
+
+set.seed(2022)
+train = sample.split(Y = dt2$readmitted, SplitRatio = 0.7)
+trainset = subset(dt2, train == T)
+testset = subset(dt2, train == F)
+#remove patient number 
+trainset = trainset [, c(-1)]
+testset = testset [, c(-1)]
+
+y = trainset$readmitted
+x = trainset[,names(trainset) != "readmitted"]
+set.seed(1)  #for bootstrap sampling & RSF selection
+rf <- randomForest(x, y = y, ntree = 500, importance = T, do.trace = TRUE)
+saveRDS(rf, file = 'random-forest-model.rds')
+
+#metrics of trainset
+rf <- readRDS('random-forest-model.rds')
+rf  #using ntree = 500 & default RSF size = floor(sqrt(ncol(x))) = 4
+  # OOB error rate = 24.83%
+  # false negative rate = 18.32%
+  # false positive rate = 31.32%
+plot(rf)
+var.impt <- importance(rf)
+var.impt
+varImpPlot(rf, type = 1) #mean decrease in accuracy
+varImpPlot(rf, type = 2) #mean decrease in gini
+
+#predict on testset
+rf.pred <- predict(rf, newdata = testset, type = 'class')
+confusionMatrix(rf.pred, reference = testset$readmitted)
