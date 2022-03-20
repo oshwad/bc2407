15854b1367e152c7bb86bc42d42813e6b5752aab
LauShingHung <92658081+LauShingHung@users.noreply.github.com>
2022-03-20 17:28:46 +0800

    Update 4. neural network.R

--- 4. neural network.R
+++ 4. neural network.R
@@ -1,12 +1,7 @@
 library(data.table)
-library("ROSE")
+
 library(neuralnet)
 
-setwd("~/GitHub/bc2407")
-dt1 <- fread('dt1-cleaned.csv', stringsAsFactors = T)
-dt1[,c('admission_type_id', 'admission_source_id', 'readmitted', 'repeat_patient')] <- lapply(dt1[,c('admission_type_id', 'admission_source_id', 'readmitted', 'repeat_patient')], factor)
-dt2 <- ROSE(readmitted~., data = dt1, N = nrow(dt1), seed=111)$data
-table(dt2$readmitted)
 
 #Neural Network
 set.seed(2014) 
