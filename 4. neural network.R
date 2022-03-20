287a77692e1072181c883b5fc28b5eac1d7b64a6
LauShingHung <92658081+LauShingHung@users.noreply.github.com>
2022-03-20 17:23:54 +0800

    Update 4. neural network.R

--- 4. neural network.R
+++ 4. neural network.R
@@ -11,67 +11,68 @@ table(dt2$readmitted)
 #Neural Network
 set.seed(2014) 
 
-m1 <- model.matrix(  
-  ~ time_in_hospital + num_lab_procedures + num_procedures + num_medications + number_outpatient + number_emergency + number_inpatient + number_diagnoses + gender + age + change + A1Cresult + diabetesMed + insulin + metformin + glipizide + glyburide + pioglitazone + admission_type_id + admission_source_id,  
-  data = dt1
-)
-new_df <- as.data.frame(m1)
-lapply(new_df,as.numeric)
-new_df['readmitted'] <- dt1$readmitted
-new_df$readmitted = dt1$readmitted
+cols.fac <- c("gender","age","admission_type_id","admission_source_id","A1Cresult",
+              "metformin","glipizide","glyburide",
+              "pioglitazone","insulin","change","diabetesMed","readmitted")
 
-dt2 <- new_df %>% group_by() %>% slice_sample(n=1000)
+#convert to factor
+for(col in cols.fac)
+  set(dt1, j = col, value = as.factor(dt1[[col]]))
 
-
-## SPLIT New Dataset into Train & Test Sets ##
-train=sample.split(Y=dt2$readmitted,SplitRatio = 0.7)
-trainset=subset(dt2,train==T)
-testset=subset(dt2,train==F)
-
-## CHECK Distribution of Train & Test Sets ##
-summary(trainset$readmitted) 
-summary(testset$readmitted)
+#create dummy variables
+dt1 <- dummy_cols(dt1, select_columns = cols.fac,remove_selected_columns = TRUE,remove_first_dummy=TRUE)
 
 
-nn1 <- neuralnet(readmitted ~ age1020 + age2030 + age3040 + age4050 + age5060 + age6070 + age7080 + age8090 + age90100 + num_procedures + time_in_hospital + num_medications + insulinNo + insulinUp + insulinSteady,  data=trainset, hidden=2, err.fct="ce", linear.output=FALSE)
+# replacing column names
+colnames(new_df)[11] <- "age1020"
+colnames(new_df)[12] <- "age2030"
+colnames(new_df)[13] <- "age3040"
+colnames(new_df)[14] <- "age4050"
+colnames(new_df)[15] <- "age5060"
+colnames(new_df)[16] <- "age6070"
+colnames(new_df)[17] <- "age7080"
+colnames(new_df)[18] <- "age8090"
+colnames(new_df)[19] <- "age90100"
+colnames(new_df)[21] <- "A1Cresultmorethan8"
+colnames(new_df)[1] <- "Intercept"
 
 
-par(mfrow=c(1,1))
-plot(nn1)
-
-nn1$net.result  # predicted outputs
-nn1$result.matrix  # summary1$startweights
-nn1$weights
-
-# The generalized weight is defined as the contribution of the ith input variable to the log-odds:
-nn1$generalized.weights
-## Easier to view GW as plots instead
-
-par(mfrow=c(2,2))
-gwplot(nn1,selected.covariate=1)
-gwplot(nn1,selected.covariate=2)
-gwplot(nn1,selected.covariate=3)
-gwplot(nn1,selected.covariate=4)
-gwplot(nn1,selected.covariate=5)
-gwplot(nn1,selected.covariate=6)
-gwplot(nn1,selected.covariate=7)
-gwplot(nn1,selected.covariate=8)
-gwplot(nn1,selected.covariate=9)
-gwplot(nn1,selected.covariate=10)
-gwplot(nn1,selected.covariate=11)
+#start neuralnet model
+set.seed(200)
+nndt <- subset(dt1,select=c("num_procedures", "time_in_hospital", "num_medications", "insulin_No","insulin_Up", "insulin_Steady" ,"readmitted_0"))
 
 
-# Display inputs with model outputs
-out <- cbind(nn1$covariate, nn1$net.result[[1]])
-dimnames(out) <- list(NULL, c("time_in_hospital","num_medications","insulinNo","insulinUp","insulinSteady"))
-head(out) 
+train <- sample.split(Y = nndt$readmitted_0, SplitRatio = 0.7)
+trainset <- subset(nndt, train == T)
+testset <- subset(nndt, train == F)
+x_train <- subset(subset(nndt, select = -c(readmitted_0)), train == T)
+x_test <- subset(subset(nndt, select = -c(readmitted_0)), train == F)
+y_train <- subset(subset(nndt, select = c(readmitted_0)), train == T)
+y_test <- subset(subset(nndt, select = c(readmitted_0)), train == F)
 
+trainSize<-500
 
+nnModel <- neuralnet(readmitted_0~ ., data = trainset[1:trainSize], hidden = c(2), err.fct = "ce", linear.output = FALSE)
 
-#Confusion matrix. model accuracy for Neural Network
-nn1.results <- compute(nn1, testset)
-pred <- nn1.results$net.result
-pred2 <- ifelse(pred>0.5, 1, 0)
-ConfusionMatrixNN <- table(pred2[,1], testset$readmitted)
-ConfusionMatrixNN 
+nnModel$startweights   # starting weights used
+nnModel$weights        # Final optimised weights
 
+unique(nnModel$net.result)    # predicted outputs. 
+nnModel$result.matrix  # summary.
+
+# View final weights Neural Network diagram
+plot(nnModel)
+
+# Prediction
+nnPred <- predict(nnModel,x_test)
+nnPred
+# Test set Confusion matrix
+nnPredCases <- as.numeric(nnPred>0.5)
+t <- table(nnPredCases,y_test$readmitted_0)
+confusionMatrix(t)
+
+# Train set Confusion Matrix
+predicted.cases <- ifelse(unlist(nnModel$net.result[[1]][,2]) > 0.5, 1, 0)  
+length(predicted.cases)
+t <- table(predicted.cases,y_test$readmitted_Yes[1:trainSize])  
+confusionMatrix(t)
