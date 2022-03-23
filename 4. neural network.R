f377263f13625bd721fe817fbc31b9bc3248088c
LauShingHung <92658081+LauShingHung@users.noreply.github.com>
2022-03-23 17:50:51 +0800

    Update 4. neural network.R

--- 4. neural network.R
+++ 4. neural network.R
@@ -4,7 +4,7 @@ library(neuralnet)
 
 
 #Neural Network
-set.seed(2014) 
+set.seed(123) 
 
 cols.fac <- c("gender","age","admission_type_id","admission_source_id","A1Cresult",
               "metformin","glipizide","glyburide",
@@ -18,23 +18,23 @@ for(col in cols.fac)
 dt1 <- dummy_cols(dt1, select_columns = cols.fac,remove_selected_columns = TRUE,remove_first_dummy=TRUE)
 
 
-# replacing column names
-colnames(new_df)[11] <- "age1020"
-colnames(new_df)[12] <- "age2030"
-colnames(new_df)[13] <- "age3040"
-colnames(new_df)[14] <- "age4050"
-colnames(new_df)[15] <- "age5060"
-colnames(new_df)[16] <- "age6070"
-colnames(new_df)[17] <- "age7080"
-colnames(new_df)[18] <- "age8090"
-colnames(new_df)[19] <- "age90100"
-colnames(new_df)[21] <- "A1Cresultmorethan8"
-colnames(new_df)[1] <- "Intercept"
+# # replacing column names
+# colnames(new_df)[11] <- "age1020"
+# colnames(new_df)[12] <- "age2030"
+# colnames(new_df)[13] <- "age3040"
+# colnames(new_df)[14] <- "age4050"
+# colnames(new_df)[15] <- "age5060"
+# colnames(new_df)[16] <- "age6070"
+# colnames(new_df)[17] <- "age7080"
+# colnames(new_df)[18] <- "age8090"
+# colnames(new_df)[19] <- "age90100"
+# colnames(new_df)[21] <- "A1Cresultmorethan8"
+# colnames(new_df)[1] <- "Intercept"
 
 
 #start neuralnet model
-set.seed(200)
-nndt <- subset(dt1,select=c("num_procedures", "time_in_hospital", "num_medications", "insulin_No","insulin_Up", "insulin_Steady" ,"readmitted_0"))
+set.seed(123)
+nndt <- subset(dt1,select=c("diabetesMed_Yes", "insulin_No","insulin_Steady","admission_type_id_3","admission_source_id_3","admission_source_id_4","admission_source_id_6","time_in_hospital","num_procedures","number_inpatient","number_diagnoses" ,"readmitted_0"))
 
 
 train <- sample.split(Y = nndt$readmitted_0, SplitRatio = 0.7)
@@ -45,7 +45,7 @@ x_test <- subset(subset(nndt, select = -c(readmitted_0)), train == F)
 y_train <- subset(subset(nndt, select = c(readmitted_0)), train == T)
 y_test <- subset(subset(nndt, select = c(readmitted_0)), train == F)
 
-trainSize<-500
+trainSize<-4000
 
 nnModel <- neuralnet(readmitted_0~ ., data = trainset[1:trainSize], hidden = c(2), err.fct = "ce", linear.output = FALSE)
 
@@ -67,7 +67,8 @@ t <- table(nnPredCases,y_test$readmitted_0)
 confusionMatrix(t)
 
 # Train set Confusion Matrix
-predicted.cases <- ifelse(unlist(nnModel$net.result[[1]][,2]) > 0.5, 1, 0)  
+predicted.cases <- ifelse(unlist(nnModel$net.result[[1]][,1]) > 0.5, 1, 0)  
 length(predicted.cases)
-t <- table(predicted.cases,y_test$readmitted_Yes[1:trainSize])  
+t <- table(predicted.cases,y_test$readmitted_0[1:trainSize])  
 confusionMatrix(t)
+
