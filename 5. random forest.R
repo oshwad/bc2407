2be19ac313ab327d5b81e3a86df5771b2c066eb3
oshwad <lkl.faith05@gmail.com>
2022-03-26 18:07:15 +0800

    rf

--- 5. random forest.R
+++ 5. random forest.R
@@ -1,35 +1,24 @@
 library(data.table)
-library("ROSE")
 library(caTools)
 library(randomForest)
 library(caret)
 
 setwd("~/GitHub/bc2407")
-dt1 <- fread('dt1-cleaned.csv', stringsAsFactors = T)
-dt1[,c('admission_type_id', 'admission_source_id', 'readmitted', 'repeat_patient')] <- lapply(dt1[,c('admission_type_id', 'admission_source_id', 'readmitted', 'repeat_patient')], factor)
-dt2 <- ROSE(readmitted~., data = dt1, N = nrow(dt1), seed=111)$data
-table(dt2$readmitted)
+trainset <- fread('trainset.csv', stringsAsFactors = T)
+trainset[,c('admission_type_id', 'admission_source_id', 'readmitted')] <- lapply(trainset[,c('admission_type_id', 'admission_source_id', 'readmitted')], factor)
+testset <- fread('testset.csv', stringsAsFactors = T)
+testset[,c('admission_type_id', 'admission_source_id', 'readmitted')] <- lapply(testset[,c('admission_type_id', 'admission_source_id', 'readmitted')], factor)
 
-set.seed(2022)
-train = sample.split(Y = dt2$readmitted, SplitRatio = 0.7)
-trainset = subset(dt2, train == T)
-testset = subset(dt2, train == F)
-#remove patient number 
-trainset = trainset [, c(-1)]
-testset = testset [, c(-1)]
-
-y = trainset$readmitted
-x = trainset[,names(trainset) != "readmitted"]
 set.seed(1)  #for bootstrap sampling & RSF selection
-rf <- randomForest(x, y = y, ntree = 500, importance = T, do.trace = TRUE)
+rf <- randomForest(readmitted ~ ., data = trainset, ntree = 500, importance = T, do.trace = TRUE)
 saveRDS(rf, file = 'random-forest-model.rds')
 
 #metrics of trainset
 rf <- readRDS('random-forest-model.rds')
 rf  #using ntree = 500 & default RSF size = floor(sqrt(ncol(x))) = 4
-  # OOB error rate = 24.83%
-  # false negative rate = 18.32%
-  # false positive rate = 31.32%
+  # OOB error rate = 41.78%
+  # false negative rate = 44.70%
+  # false positive rate = 38.87%
 plot(rf)
 var.impt <- importance(rf)
 var.impt
@@ -42,29 +31,52 @@ confusionMatrix(rf.pred, reference = testset$readmitted)
 
 
 
-########################################################VERSION 2############################
+
+# fixing the new factors error -----
+for (i in 1:ncol(testset)){
+  print(summary(testset[[i]]))
+  print(summary(us[[i]]))
+}
+
+summary(testset$admission_source_id)
+summary(us$admission_source_id)
+summary(testset$admission_type_id)
+summary(us$admission_type_id)
+
+testset <- testset[testset$admission_source_id != '10',]
+testset <- testset[testset$admission_type_id != '7',]
+testset <- droplevels(testset)
+
+testset <- rbind(ds[1, ] , testset)
+testset <- testset[-1,]
 
 
 
 
-dt3 <- dt1
-dt3 <- dt3 %>% group_by() %>% slice_sample(n=2000)
-## SPLIT New Dataset into Train & Test Sets ##
-train=sample.split(Y=dt3$readmitted,SplitRatio = 0.7)
-trainset=subset(dt3,train==T)
-testset=subset(dt3,train==F)
+# down/up sample function -----
 
-m.rf <- randomForest(readmitted ~ . , data=trainset, importance=T)
-m.rf
+y = 'readmitted'
+x = (names(trainset) != "readmitted")
 
-#error plot
-plot(m.rf)
-#if categorical, there is FP, FN, overall error
+#downSample function
+set.seed(1)
+ds <- downSample(trainset[,..x], trainset$readmitted, yname = 'readmitted')
+summary(ds)
+set.seed(1)  #for bootstrap sampling & RSF selection
+rf_ds <- randomForest(readmitted ~ ., data = ds, ntree = 500, importance = T, do.trace = TRUE)
+rf_ds
+rf_pred_ds <- predict(rf_ds, newdata = testset, type = 'class')
+confusionMatrix(rf_pred_ds, reference = testset$readmitted)
 
-m.rf.y <- predict(m.rf, newdata = testset)
-ConfusionMatrixRF <- table(m.rf.y,testset$readmitted)
-ConfusionMatrixRF
+#upSample function
+set.seed(1)
+us <- upSample(trainset[,..x], trainset$readmitted, yname = 'readmitted')
+summary(us)
+set.seed(1)  #for bootstrap sampling & RSF selection
+rf_us <- randomForest(readmitted ~ ., data = us, ntree = 500, importance = T, do.trace = TRUE)
+rf_us
+rf_pred_us <- predict(rf_us, newdata = testset, type = 'class')
+confusionMatrix(rf_pred_us, reference = testset$readmitted)
 
 
-var.impt.rf <- importance(m.rf)
-print(var.impt.rf)
+
