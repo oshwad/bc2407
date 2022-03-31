fbd507eee02b5e5577e14ddfbac4db09e182be29
waidee <101866196+waidee@users.noreply.github.com>
2022-03-31 12:10:19 +0800

    Update 1. data preparation and cleaning.R

--- 1. data preparation and cleaning.R
+++ 1. data preparation and cleaning.R
@@ -228,13 +228,8 @@ testset = subset(dt1, train == F)
 
 #correct class imbalance in train set -> sampling from majority
 set.seed(2022)
-majority = trainset[trainset$readmitted==0]
-minority = trainset[trainset$readmitted==1]
-sample_rows = sample(1:nrow(majority), size=nrow(minority))
-trainset_balanced = majority[sample_rows,]
-trainset_balanced = rbind(minority, trainset_balanced)
-summary(trainset_balanced)
-trainset <- trainset_balanced
+ds <- downSample(trainset[,1:20], trainset$readmitted, yname = 'readmitted')
+trainset <- ds
 
 #export dt1, testset, and trainset
 fwrite(dt1, file = 'dt1-cleaned.csv')
