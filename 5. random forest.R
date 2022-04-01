9f224c95cd0119fce64eb2a6ee0bef76dbf2a438
oshwad <lkl.faith05@gmail.com>
2022-04-01 17:29:21 +0800

    update random forest

--- 5. random forest.R
+++ 5. random forest.R
@@ -9,7 +9,7 @@ trainset[,c('admission_type_id', 'admission_source_id', 'readmitted')] <- lapply
 testset <- fread('testset.csv', stringsAsFactors = T)
 testset[,c('admission_type_id', 'admission_source_id', 'readmitted')] <- lapply(testset[,c('admission_type_id', 'admission_source_id', 'readmitted')], factor)
 
-set.seed(1)  #for bootstrap sampling & RSF selection
+set.seed(2022)  #for bootstrap sampling & RSF selection
 rf <- randomForest(readmitted ~ ., data = trainset, ntree = 500, importance = T, do.trace = TRUE)
 saveRDS(rf, file = 'random-forest-model.rds')
 
@@ -25,58 +25,24 @@ var.impt
 varImpPlot(rf, type = 1) #mean decrease in accuracy
 varImpPlot(rf, type = 2) #mean decrease in gini
 
-#predict on testset
-rf.pred <- predict(rf, newdata = testset, type = 'class')
-confusionMatrix(rf.pred, reference = testset$readmitted)
-
-
-
-
 # fixing the new factors error -----
 for (i in 1:ncol(testset)){
   print(summary(testset[[i]]))
-  print(summary(us[[i]]))
+  print(summary(trainset[[i]]))
 }
 
 summary(testset$admission_source_id)
-summary(us$admission_source_id)
+summary(trainset$admission_source_id)
 summary(testset$admission_type_id)
-summary(us$admission_type_id)
+summary(trainset$admission_type_id)
 
 testset <- testset[testset$admission_source_id != '10',]
 testset <- testset[testset$admission_type_id != '7',]
 testset <- droplevels(testset)
 
-testset <- rbind(ds[1, ] , testset)
+testset <- rbind(trainset[1, ], testset)
 testset <- testset[-1,]
 
-
-
-
-# down/up sample function -----
-
-y = 'readmitted'
-x = (names(trainset) != "readmitted")
-
-#downSample function
-set.seed(1)
-ds <- downSample(trainset[,..x], trainset$readmitted, yname = 'readmitted')
-summary(ds)
-set.seed(1)  #for bootstrap sampling & RSF selection
-rf_ds <- randomForest(readmitted ~ ., data = ds, ntree = 500, importance = T, do.trace = TRUE)
-rf_ds
-rf_pred_ds <- predict(rf_ds, newdata = testset, type = 'class')
-confusionMatrix(rf_pred_ds, reference = testset$readmitted)
-
-#upSample function
-set.seed(1)
-us <- upSample(trainset[,..x], trainset$readmitted, yname = 'readmitted')
-summary(us)
-set.seed(1)  #for bootstrap sampling & RSF selection
-rf_us <- randomForest(readmitted ~ ., data = us, ntree = 500, importance = T, do.trace = TRUE)
-rf_us
-rf_pred_us <- predict(rf_us, newdata = testset, type = 'class')
-confusionMatrix(rf_pred_us, reference = testset$readmitted)
-
-
-
+#predict on testset
+rf.pred <- predict(rf, newdata = testset, type = 'class')
+confusionMatrix(rf.pred, reference = testset$readmitted)
