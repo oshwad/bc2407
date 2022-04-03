612bf1b29930ec4e0a9f2f7f811ddf3d8b6a8412
LauShingHung <92658081+LauShingHung@users.noreply.github.com>
2022-04-03 21:28:35 +0800

    Update 4. Neural Network.R

--- 4. Neural Network.R
+++ 4. Neural Network.R
@@ -4,18 +4,43 @@ library(fastDummies)
 library(caret)
 
 setwd("~/GitHub/bc2407")
-trainset <- fread('data-trainset.csv', stringsAsFactors = T)
-trainset[,c('admission_type_id', 'admission_source_id', 'readmitted')] <- lapply(trainset[,c('admission_type_id', 'admission_source_id', 'readmitted')], factor)
-testset <- fread('data-testset.csv', stringsAsFactors = T)
-testset[,c('admission_type_id', 'admission_source_id', 'readmitted')] <- lapply(testset[,c('admission_type_id', 'admission_source_id', 'readmitted')], factor)
+set.seed(2022) 
+
+train = sample.split(Y = dt1$readmitted, SplitRatio = 0.9)
+trainset = subset(dt1, train == T)
+testset = subset(dt1, train == F)
+
+#correct class imbalance in train set -> sampling from majority
+trainset <- downSample(trainset[,1:20], trainset$readmitted, yname = 'readmitted')
+
+summary(trainset$readmitted)
 
 cols.fac <- c("gender","age","admission_type_id","admission_source_id","A1Cresult",
               "metformin","glipizide","glyburide",
               "pioglitazone","insulin","change","diabetesMed","readmitted")
 
+for(col in cols.fac)
+  set(trainset, j = col, value = as.factor(trainset[[col]]))
+
+for(col in cols.fac)
+  set(testset, j = col, value = as.factor(testset[[col]]))
 #create dummy variables
 trainset <- dummy_cols(trainset, select_columns = cols.fac,remove_selected_columns = TRUE,remove_first_dummy = T)
 testset <- dummy_cols(testset, select_columns = cols.fac,remove_selected_columns = TRUE,remove_first_dummy = T)
+
+trainset[,"readmitted_1"] <- 0
+testset[,"readmitted_1"] <- 0
+for (i in 1:nrow(trainset)) {
+  if (trainset$readmitted_0[i] == 0) {
+    trainset$readmitted_1[i] = 1
+  }
+}
+
+for (i in 1:nrow(testset)) {
+  if (testset$readmitted_0[i] == 0) {
+    testset$readmitted_1[i] = 1
+  }
+}
 
 #replacing column names
 count = 10
@@ -37,6 +62,12 @@ set.seed(2022)
 nnModel <- neuralnet(readmitted_1~ age1020+age2030+age3040+age4050+age5060+age6070+age7080+age8090+age90100+diabetesMed_Yes+insulin_No+insulin_Steady+insulin_Up+time_in_hospital+num_procedures+num_medications+number_emergency+number_inpatient+number_diagnoses, data = trainset[sample_rows,], hidden = c(2,1),act.fct="tanh", linear.output = FALSE,stepmax=1e7)
 
 saveRDS(nnModel, file = 'r-neural-network-model.rds')
+
+#Run 3 other models with different number of hidden layers and hidden nodes
+#nnModel <- neuralnet(readmitted_1~ age1020+age2030+age3040+age4050+age5060+age6070+age7080+age8090+age90100+diabetesMed_Yes+insulin_No+insulin_Steady+insulin_Up+time_in_hospital+num_procedures+num_medications+number_emergency+number_inpatient+number_diagnoses, data = trainset[sample_rows,], hidden = c(2),act.fct="tanh", linear.output = FALSE,stepmax=1e7)
+#nnModel <- neuralnet(readmitted_1~ age1020+age2030+age3040+age4050+age5060+age6070+age7080+age8090+age90100+diabetesMed_Yes+insulin_No+insulin_Steady+insulin_Up+time_in_hospital+num_procedures+num_medications+number_emergency+number_inpatient+number_diagnoses, data = trainset[sample_rows,], hidden = c(1),act.fct="tanh", linear.output = FALSE,stepmax=1e7)
+#nnModel <- neuralnet(readmitted_1~ age1020+age2030+age3040+age4050+age5060+age6070+age7080+age8090+age90100+diabetesMed_Yes+insulin_No+insulin_Steady+insulin_Up+time_in_hospital+num_procedures+num_medications+number_emergency+number_inpatient+number_diagnoses, data = trainset[sample_rows,], hidden = c(1,1),act.fct="tanh", linear.output = FALSE,stepmax=1e7)
+
 nnModel <- readRDS('r-neural-network-model.rds')
 
 nnModel$startweights   # starting weights used
